d05601174cb87bd07838e9ffc87e49145bfbaaf8
Gavrilin Evgeny <egavrilin@oaorti.ru>
2019-09-20 20:26:00 +0300

    Revert "remove all files for review"
    This reverts commit 40d288b61b391265f37a4f26cf482992724f3943.

---
+++ lib.cpp
@@ -0,0 +1,53 @@
+
+#include "lib.h"
+#include <algorithm>
+#include <cassert>
+
+namespace my_lib {
+	
+std::vector<std::string> split(const std::string &str, char d)
+{
+  std::vector<std::string> r;
+
+  std::string::size_type start = 0;
+  std::string::size_type stop = str.find_first_of(d);
+  while ( stop != std::string::npos ) {
+    r.push_back(str.substr(start, stop - start));
+
+    start = stop + 1;
+    stop = str.find_first_of(d, start);
+  }
+  r.push_back(str.substr(start));
+  return r;
+}
+
+std::vector<int> filter(const ip_pool_t& ip_pool, const std::string& s1, 
+  const std::string&s2)
+{
+  std::vector<int> indexes;
+  for ( ip_pool_t::size_type index = 0; index < ip_pool.size(); index++ ) {
+    const ip_t& ip = ip_pool[index];     assert(ip.size() == 4);
+    if ( ip[0] == s1 ) {
+      if ( s2.empty() ) {
+        indexes.push_back(index);
+      } else if ( ip[1] == s2) {
+        indexes.push_back(index);
+      }
+    }
+  }
+  return indexes;
+}
+
+std::vector<int> filter_any(const ip_pool_t& ip_pool, const std::string& str)
+{
+  std::vector<int> indexes;
+  for ( ip_pool_t::size_type index = 0; index < ip_pool.size(); index++ ) {
+    const ip_t& ip = ip_pool[index];     assert(ip.size() == 4);
+    auto it = std::find(ip.cbegin(), ip.cend(), str);
+    if ( it != ip.cend() )
+      indexes.push_back(index);
+  }
+  return indexes;
+}
+
+}
---
+++ lib.h
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+namespace my_lib {
+	
+using ip_t = std::vector<std::string>;
+using ip_pool_t = std::vector<ip_t>;
+
+// ("",  '.') -> [""]
+// ("11", '.') -> ["11"]
+// ("..", '.') -> ["", "", ""]
+// ("11.", '.') -> ["11", ""]
+// (".11", '.') -> ["", "11"]
+// ("11.22", '.') -> ["11", "22"]
+std::vector<std::string> split(const std::string &str, char d);
+
+// фильтрация по первому байту и по второму, если задан
+std::vector<int> filter(const ip_pool_t& ip_pool, const std::string& s1, const std::string&s2 = std::string());
+
+// фильтрация по любому байту
+std::vector<int> filter_any(const ip_pool_t& ip_pool, const std::string& str);
+
+}
